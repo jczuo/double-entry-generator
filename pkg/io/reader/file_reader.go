44a6d8425e194e0a2d9f05130a438c54af0b9b4e
jczuo <jianchun.zuo@gmail.com>
2025-05-05 12:30:34 +0800

    增加对csv文件的预处理，包括双引号、制表符等
    这个新版本的主要改进是：
    自动处理微信支付账单文件中的双引号和制表符
    不需要手动预处理文件
    使用流式处理，内存使用更高效
    
    Signed-off-by: jczuo <jianchun.zuo@gmail.com>

--- pkg/io/reader/file_reader.go
+++ pkg/io/reader/file_reader.go
@@ -4,8 +4,10 @@
 package reader
 
 import (
+	"bufio"
 	"io"
 	"os"
+	"strings"
 
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
@@ -27,4 +29,33 @@ func GetGBKReader(filename string) (io.Reader, error) {
 
 	gbkReader := transform.NewReader(csvFile, simplifiedchinese.GBK.NewDecoder())
 	return gbkReader, nil
+}
+
+// GetWechatBillReader 处理微信支付账单文件，移除双引号并将制表符替换为逗号
+func GetWechatBillReader(filename string) (io.Reader, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// 创建一个管道
+	pr, pw := io.Pipe()
+
+	// 在后台处理文件
+	go func() {
+		defer file.Close()
+		defer pw.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			// 移除双引号并替换制表符为逗号
+			line = strings.ReplaceAll(line, "\"", "")
+			line = strings.ReplaceAll(line, "\t", ",")
+			// 写入处理后的行
+			pw.Write([]byte(line + "\n"))
+		}
+	}()
+
+	return pr, nil
 }
